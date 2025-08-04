28722e0072c17519e85f7abb377229482566af03
dhdlqns12 <dhdlqns12@gmail.com>
2025-08-05 07:36:02 +0900

    Create 1days.cs

---
+++ Baekjoon/1days.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+//namespace B5_2557
+//{
+//    public class program
+//    {
+//        public static void Main()
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
+
+//namespace b5_1000
+//{
+//    class program
+//    {
+//        static void main()
+//        {
+//            string s = console.readline();
+//            string[] ss = s.split();
+//            int a = int.parse(ss[0]);
+//            int b = int.parse(ss[1]);
+//            console.writeline(a + b);
+//        }
+//    }
+//}
+
+//namespace b5_1001
+//{
+//    class program
+//    {
+//        static void main()
+//        {
+//            string s = console.readline();
+//            string[] ss = s.split();
+//            int n = int.parse(ss[0]);
+//            int m = int.parse(ss[1]);
+
+//            console.writeline(n - m);
+//        }
+//    }
+//}
+
+//namespace B5_10998
+//{
+//    class program
+//    {
+//        static void Main()
+//        {
+//            string s = Console.ReadLine();
+//            string[] ss = s.Split();
+
+//            int n = int.Parse(ss[0]);
+//            int m = int.Parse(ss[1]);
+
+//            int result = n * m;
+//            Console.WriteLine(result);
+//        }
+//    }
+//}
+
+//namespace B5_1008
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            string s = Console.ReadLine();
+//            string[] ss = s.Split();
+
+//            double n = double.Parse(ss[0]);
+//            double m = double.Parse(ss[1]);
+
+//            double result = n / m;
+
+//            Console.WriteLine(result);
+//        }
+//    }
+//}
+
+namespace B5_10869
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+
+            int a = int.Parse(ss[0]);
+            int b = int.Parse(ss[1]);
+
+            int sum = a + b;
+            int dif = a - b;
+            int mul = a * b;
+            int div = a / b;
+            int mod = a % b;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(dif);
+            Console.WriteLine(mul);
+            Console.WriteLine(div);
+            Console.WriteLine(mod);
+        }
+    }
+}
