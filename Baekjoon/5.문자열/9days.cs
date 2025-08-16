7d0127937c24b8f26179112163f80ffbc42e4a20
dhdlqns12 <dhdlqns12@gmail.com>
2025-08-16 10:40:54 +0900

    Create 9days.cs

---
+++ Baekjoon/5.문자열/9days.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//namespace B5_27866
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            char result = GetChar();
+
+//            Console.WriteLine($"{result}");
+//        }
+
+//        static char GetChar()
+//        {
+//            string input = Console.ReadLine();
+//            int index = int.Parse(Console.ReadLine());
+
+//            return input[index-1];
+//        }
+//    }
+//}
+
+//namespace B5_2743
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            int result = GetLength();
+
+//            Console.WriteLine($"{result}");
+//        }
+
+//        static int GetLength()
+//        {
+//            string input = Console.ReadLine();
+//            int count = input.Length;
+
+//            return count;
+//        }
+//    }
+//}
+
+//namespace B5_9086
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            int num = int.Parse(Console.ReadLine());
+
+
+//            for (int i = 0; i < num; i++)
+//            {
+//                string input = Console.ReadLine();
+//                GetStartEnd(input);
+//            }
+//        }
+
+//        public static void GetStartEnd(string text)
+//        {
+//            char a = text.First();
+//            char b = text.Last();
+
+//            Console.WriteLine($"{a}{b}");
+//        }
+//    }
+//}
