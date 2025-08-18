fb45466879b45da79cf81095491bcf402aa631ba
dhdlqns12 <dhdlqns12@gmail.com>
2025-08-18 19:22:23 +0900

    Create 10days.cs

---
+++ Baekjoon/5.문자열/10days.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace B5_11654
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            char a = char.Parse(Console.ReadLine());
+
+//            Console.WriteLine((int)a);
+//        }
+//    }
+//}
+
+//namespace B4_11720
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            int n = int.Parse(Console.ReadLine());
+//            string num = Console.ReadLine();
+
+//            int sum = 0;
+//            for (int i = 0; i < n; i++)
+//            {
+//                sum += num[i] - '0'; //ASCII 코드 기반해서 '0'은 48인데 각 숫자에서 아스키코드의 '0'을 빼주면 원래 숫자값이 된다.
+//            }
+
+//            Console.WriteLine($"{sum}");
+//        }
+//    }
+//}
+
