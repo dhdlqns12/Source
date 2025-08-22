92e19d3e7a09b93d976bc452ac54cd6d35e558c5
dhdlqns12 <dhdlqns12@gmail.com>
2025-08-22 21:16:59 +0900

    Create 14days.cs

---
+++ Baekjoon/6.심화1/14days.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace B5_25083
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("         ,r'\"7");
+//            Console.WriteLine("r`-_   ,'  ,/");
+//            Console.WriteLine(" \\. \". L_r'");
+//            Console.WriteLine("   `~\\/");
+//            Console.WriteLine("      |");
+//            Console.WriteLine("      |");
+//        }
+//    }
+//}
+
+//namespace B5_3003
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            int[] chess = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+//            int[] complete = { 1, 1, 2, 2, 2, 8 };
+
+//            for (int i = 0; i < 6; i++)
+//            {
+//                Console.Write($"{complete[i] - chess[i]} ");
+//            }
+//        }
+//    }
+//}
