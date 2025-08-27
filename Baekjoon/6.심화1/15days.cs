274cb22ac2393c522f6684cb4d5722537060ae3e
dhdlqns12 <dhdlqns12@gmail.com>
2025-08-28 01:03:21 +0900

    Create 15days.cs

---
+++ Baekjoon/6.심화1/15days.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace B3_2444
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            int num = int.Parse(Console.ReadLine());
+
+//            for (int i = 1; i <= num; i++)
+//            {
+//                Console.WriteLine(new string(' ', num - i) + new string('*', 2 * i - 1));
+//            }
+
+//            for (int i = num - 1; i >= 1; i--)
+//            {
+//                Console.WriteLine(new string(' ', num - i) + new string('*', 2 * i - 1));
+//            }
+//        }
+//    }
+//}
+
+//namespace B3_10988
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            var input = Console.ReadLine();
+//            var reverse = new string(input.Reverse().ToArray());
+
+//            if (input == reverse)            
+//                Console.WriteLine("1");
+//            else
+//                Console.WriteLine("0");
+//        }
+//    }
+//}
